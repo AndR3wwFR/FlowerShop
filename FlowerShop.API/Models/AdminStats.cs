cd608053512ed5643ce4a7d446b7427848dc8024
catdog <228p@BTK.MAIN>
2026-05-12 14:19:02 +0300

    Добавление gitignore. Добавление моделей в проект

---
+++ FlowerShop.API/Models/AdminStats.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowerShop.API.Models
+{
+    public class AdminStats
+    {
+        public decimal TotalRevenue { get; set; }
+        public int OrderCount
+        {
+            get; set;
+        }
+    }
+}
---
+++ FlowerShop.API/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿namespace FlowerShop.API.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Phone { get; set; }
+
+        public string Address {  get; set; }
+        public string DeliveryTime { get; set; }
+
+        public List<string> Items { get; set; } = new();
+        public decimal TotalPrice { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.New;
+
+    }
+
+    public enum OrderStatus
+    {
+        New,
+        Assembling,
+        Ready,
+        Delivering,
+        Complited
+    }
+}
